29ab45c34da363f60777ee73812c00fa6981f8d5
eustache <eustache@diemert.fr>
2012-09-12 17:50:48 +0200

    reworked cascading model

--- features.py
+++ features.py
@@ -4,7 +4,7 @@ import datetime
 import features
 import numpy as np
 import pandas as pd
-import re
+import re, sys
 import math
 
 ##############################################################
@@ -27,62 +27,71 @@ def more_than_one_paragraph(e):
         return 1.0
     return nb_display_lines / float(nb_new_lines)
 
-def nb_badges(e):
-    from badges import badges
-    return badges.get(int(e),0)
-
 def what_how(title):
     t = title.lower()
     s = 0
     if 'what' in t:
-        s+= 1
+        return 1
     if 'how' in t:
-        s+= 1e1
+        return 1e1
     if 'best' in t:
-        s+= 1e2
+        return 1e2
     if 'compare' in t:
-        s+= 1e3
+        return 1e3
     if 'why' in t:
-        s+= 1e4
+        return 1e4
     if 'favorite' in t:
-        s+= 1e5
+        return 1e5
     if 'new' in t:
-        s+= 1e6
+        return 1e6
     if 'most' in t:
-        s+= 1e7
+        return 1e7
     if 'good' in t:
-        s+= 1e8
+        return 1e8
     if 'which' in t:
-        s+= 1e9
+        return 1e9
     if 'where' in t:
-        s+= 1e10
+        return 1e10
     return s
+
+def nb_ref(txt):
+    n = 0
+    for i in range(10):
+        if '  [%d]'%i in txt:
+            n += 1
+    return n
+
+def nb_lists(txt):
+    n = 0
+    for i in range(10):
+        if '  %d. '%i in txt:
+            n += 1
+    return n
+
+def nb_qmarks(txt):
+    return len([ c for c in txt if c == '?' ])
 
 ###########################################################
 import csv, random
-def online_extract_features(fn, train=1,limit=1e5):
+def online_extract_features(fn, train, limit):
     fea = dict()
     fd = open(fn,'rb')
     reader = csv.reader(fd,
                         delimiter=',',
                         quotechar='"',
                         quoting=csv.QUOTE_MINIMAL)
-    header = None
-    for row in reader:
-        if not header:
-            header = row
-            blen_index = header.index('BodyMarkdown')
-            user_index = header.index('OwnerUserId')
-            userdate_index = header.index('OwnerCreationDate')
-            postdate_index = header.index('PostCreationDate')
-            title_index = header.index('Title')
-            tags_indexes = [ header.index('Tag%d'%i) for i in range(1,6) ]
-            reput_index = header.index('ReputationAtPostCreation')
-            if train:
-                st_index = header.index('OpenStatus')
-            break
+    header = reader.next()
+    
+    blen_index = header.index('BodyMarkdown')
+    user_index = header.index('OwnerUserId')
+    userdate_index = header.index('OwnerCreationDate')
+    postdate_index = header.index('PostCreationDate')
+    title_index = header.index('Title')
+    tags_indexes = [ header.index('Tag%d'%i) for i in range(1,6) ]
+    reput_index = header.index('ReputationAtPostCreation')
+    if train:
+        st_index = header.index('OpenStatus')
 
-    _ = 0
     for f in ('BodyCharLength',
               'BodyWordLength',
               'BodyCodeLines',
@@ -90,28 +99,37 @@ def online_extract_features(fn, train=1,limit=1e5):
               'TitleCharLength',
               'TitleLengthWords',
               'ReputationAtPostCreation',
-              'NbBadges',
               'UserAge',
               'MoreThanOneParagraph',
-              'WhatHowTitle'):
+              'WhatHowTitle',
+              'NbRef',
+              'NbLists',
+              'NbQMarks'):
         fea[f] = []
     status = {}
     status['OpenStatus'] = []
     nb_open = 0
+    nb_open_kept = 0
+    nb_other = 0
+    _i = 0
     for row in reader:
+        _i += 1
+        if _i > limit:
+            break
         if train:
             if row[st_index] == 'open':
                 nb_open += 1
-                if not random.random() < 0.50:
+                if not random.random() < 0.60:
+                    nb_open_kept += 1
                     continue
-        _ += 1
-        if not _ % 100:
-            print "\r%d"%_,
-        if _ > limit:
-            break
+            else:
+                nb_other += 1
+        print >>sys.stderr, "\ro:%d        k:%d        n:%d    i:%d"%(nb_open,
+                                                                      nb_open_kept,
+                                                                      nb_other,
+                                                                      _i),
         fea['MoreThanOneParagraph'].append(more_than_one_paragraph(row[blen_index]))
         fea['UserAge'].append(user_age(row[postdate_index],row[userdate_index]))
-        fea['NbBadges'].append(nb_badges(row[user_index]))
         fea['TitleLengthWords'].append( len(row[title_index].split(' '))  )
         fea['TitleCharLength'].append( len(row[title_index])  )
         fea['BodyCharLength'].append( len(row[blen_index])  )
@@ -120,8 +138,11 @@ def online_extract_features(fn, train=1,limit=1e5):
         fea['NumTags'].append(len([ row[i] for i in tags_indexes if len(row[i]) ]))
         fea['ReputationAtPostCreation'].append(int(row[reput_index]))
         fea['WhatHowTitle'].append( what_how(row[title_index])  )
+        fea['NbRef'].append(nb_ref(row[blen_index]))
+        fea['NbLists'].append(nb_lists(row[blen_index]))
+        fea['NbQMarks'].append(nb_qmarks(row[blen_index]))
         if train:
             status['OpenStatus'].append(row[st_index])
 
-    print nb_open,'/',_,'open'
+    print >>sys.stderr,""
     return pd.DataFrame.from_dict(fea),pd.DataFrame.from_dict(status)
